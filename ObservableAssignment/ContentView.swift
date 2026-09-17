46cbe35e2875d5937a4d5f18cc40f69282a85ecc
Ayush Gupta <320959331+ayushguptaRSL@users.noreply.github.com>
2026-09-17 10:51:59 +0530

    Initial Commit

---
+++ ObservableAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ObservableAssignment
+//
+//  Created by Ayush Gupta 2 on 17/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ObservableAssignment/ObservableAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ObservableAssignmentApp.swift
+//  ObservableAssignment
+//
+//  Created by Ayush Gupta 2 on 17/09/26.
+//
+
+import SwiftUI
+
+@main
+struct ObservableAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
